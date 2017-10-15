7f5176ca1f82f715858e72e20c829e47615c4d05
Pedro Mass <pedro.j.mass@gmail.com>
2017-10-15 08:06:55 -0400

    Bump version.

--- Hide-Kinja-Deals.user.js
+++ Hide-Kinja-Deals.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide Kinja Deals
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.1
 // @description  try to take over the world!
 // @author       You
 // @match        *.lifehacker.com/*
